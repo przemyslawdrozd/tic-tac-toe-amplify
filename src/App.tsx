9b6d4d6338d918b9244abc2142c77c437893d4b6
Przemo <przemo06133@gmail.com>
2023-05-30 18:58:25 +0200

    Add amplify UI

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Button } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  // const TTT_DATA = [
+  //   ['X', 'X', 'X'],
+  //   ['X', 'X', 'X'],
+  //   ['X', 'X', 'X'],
+  // ]
+
+  return (
+    <>
+      <h1>Game</h1>
+      <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
