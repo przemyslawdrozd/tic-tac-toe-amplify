c7e33fce70817daa8e2d5b836ab67e45290b5e82
Przemo <przemo06133@gmail.com>
2023-05-31 19:10:40 +0200

    Game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,67 @@
-import './App.css'
-import { Button } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import { useState } from 'react'
+import { Button, Grid, Flex, View, useTheme } from '@aws-amplify/ui-react'
+import { INIT_BOARD, WIN_PATTERN } from './utils/conts'
 
-function App() {
-  // const [count, setCount] = useState(0)
+const App = () => {
+  const [board, setBoard] = useState<string[]>(INIT_BOARD)
+  const [player, setPlayer] = useState<'X' | 'O'>('X')
+  const [winner, setWinner] = useState<string>('')
 
-  // const TTT_DATA = [
-  //   ['X', 'X', 'X'],
-  //   ['X', 'X', 'X'],
-  //   ['X', 'X', 'X'],
-  // ]
+  const handleClick = (index: any) => {
+    if (board[index] || winner) return
 
+    const updatedBoard = [...board]
+    updatedBoard[index] = player
+    setBoard(updatedBoard)
+
+    for (let [a, b, c] of WIN_PATTERN) {
+      if (
+        updatedBoard[a] &&
+        updatedBoard[a] === updatedBoard[b] &&
+        updatedBoard[a] === updatedBoard[c]
+      )
+        return setWinner(updatedBoard[a])
+    }
+
+    console.log('set new player')
+    setPlayer(player === 'X' ? 'O' : 'X')
+  }
+
+  const resetGame = () => {
+    setBoard(INIT_BOARD)
+    setPlayer('X')
+    setWinner('')
+  }
+
+  const { tokens } = useTheme()
   return (
-    <>
-      <h1>Game</h1>
-      <Button>Hello</Button>
-    </>
+    <Flex
+      direction='column'
+      justifyContent='flex-start'
+      alignItems='stretch'
+      alignContent='flex-start'
+      wrap='nowrap'
+      gap='1rem'>
+      <View height='32rem' width='30rem'>
+        <Grid
+          templateColumns='1fr 1fr 1fr'
+          templateRows='10rem 10rem 10rem'
+          gap={tokens.space.small}>
+          {board.map((cell, i) => (
+            <Button size='large' onClick={() => handleClick(i)}>
+              {cell}
+            </Button>
+          ))}
+        </Grid>
+      </View>
+      {winner && <div className='winner'>{`Winner: ${winner}`}</div>}
+      {!winner && board.every(value => value) && (
+        <div className='draw'>It's a draw!</div>
+      )}
+      <button className='reset-button' onClick={resetGame}>
+        Reset
+      </button>
+    </Flex>
   )
 }
 
